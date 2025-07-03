88586afe17fe3427dd4c5841e707a644dfb26dcc
Mru321 <136099768+Mru321@users.noreply.github.com>
2025-07-03 21:00:39 +0530

    inserting initial data into database

--- backend/model/PositionsModel.js
+++ backend/model/PositionsModel.js
@@ -2,6 +2,6 @@ const { model } = require("mongoose");
 
 const { PositionsSchema }= require("../schemas/PositionsSchema");
 
-const HoldingsModel = new model("position", PositionsSchema);
+const PositionsModel = new model("position", PositionsSchema);
 
-module.exports= {PositionsModel};
+module.exports= { PositionsModel };
