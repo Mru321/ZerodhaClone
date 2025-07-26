9ffa50327dfe807a2b5c039ab72e174813cb2b2e
Mru321 <136099768+Mru321@users.noreply.github.com>
2025-07-27 01:45:59 +0530

    User Authentication feature backend

--- backend/Middlewares/AuthMiddleware.js
+++ backend/Middlewares/AuthMiddleware.js
@@ -10,7 +10,8 @@ module.exports.userVerification = (req, res) => {
   jwt.verify(token, process.env.TOKEN_KEY, async (err, data) => {
     if (err) {
      return res.json({ status: false })
-    } else {
+    } 
+    else {
       const user = await User.findById(data.id)
       if (user) return res.json({ status: true, user: user.username })
       else return res.json({ status: false })
